073be41fc66c9462ab57017e17a400b8ff70dc98
bogdan9861 <besedin_b@bk.ru>
2025-01-25 19:20:11 +0300

    first

---
+++ index.js
@@ -0,0 +1,23 @@
+const io = require("socket.io")(5050, {
+  cors: {
+    origin: ["https://guileless-clafoutis-9d01ff.netlify.app/"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("send-message", (room, message, time, id, audio) => {
+    socket
+      .to(room)
+      .emit("recive-message", { message, time, userId: id, audio });
+  });
+
+  socket.on("send-status", (room, id, status) => {
+    console.log(status);
+    
+    io.emit("get-status", { writerId: id, status });
+  });
+
+  socket.on("join-room", (room) => {
+    socket.join(room);
+  });
+});
